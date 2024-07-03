23f1dbb8574ebb8a1b6a64e8e4c1e3ec2f3649c1
henrion-y <1456547497@qq.com>
2024-07-03 09:08:41 +0800

    feat: 优化jwt

--- http/gin/middlewares/jwt.middleware.go
+++ http/gin/middlewares/jwt.middleware.go
@@ -51,11 +51,8 @@ func (m *JWTAuthMiddleware) SetClaims() gin.HandlerFunc {
 // SetClaimsAbortTourist JWTAuthMiddleware 中间件，检查token 拦截游客
 func (m *JWTAuthMiddleware) SetClaimsAbortTourist() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		m.SetClaims()(ctx)
-
-		// 解析token包含的信息
-		_, exists := ctx.Get("claims")
-		if !exists {
+		authHeader := ctx.Request.Header.Get("Authorization")
+		if authHeader == "" {
 			// 拦截游客
 			ctx.JSON(http.StatusOK, gin.H{
 				"code":    -1,
@@ -64,6 +61,32 @@ func (m *JWTAuthMiddleware) SetClaimsAbortTourist() gin.HandlerFunc {
 			ctx.Abort()
 			return
 		}
+
+		// 按空格拆分
+		parts := strings.SplitN(authHeader, " ", 2)
+		if !(len(parts) == 2 && parts[0] == "Bearer") {
+			// 拦截游客
+			ctx.JSON(http.StatusOK, gin.H{
+				"code":    -1,
+				"message": "无权限访问，请求未携带token",
+			})
+			ctx.Abort()
+			return
+		}
+
+		// 解析token包含的信息
+		claims, err := m.authService.ParseToken(parts[1])
+		if err != nil {
+			// 拦截游客
+			ctx.JSON(http.StatusOK, gin.H{
+				"code":    -1,
+				"message": "无权限访问，请求未携带token",
+			})
+			ctx.Abort()
+			return
+		}
+		// 将当前请求的claims信息保存到请求的上下文c上
+		ctx.Set("claims", claims)
 		ctx.Next() // 后续的处理函数可以用过ctx.Get("claims")来获取当前请求的用户信息
 	}
 }
